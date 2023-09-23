7bebb3a5b2fe0f6c36a561d261e154d5db7871f2
Aworldc <83331712+Aworldc@users.noreply.github.com>
2023-09-23 13:35:09 +1000

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-import { ipcRenderer, contextBridge } from 'electron'
-
-ipcRenderer.on('setInnerHtml', (event, args) => {
-    document.body.innerHTML = args
-})
-
-contextBridge.exposeInMainWorld('message', (name, data) => {
-    ipcRenderer.send(`contentMessage__${name}`, data)
-})
